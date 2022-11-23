e50998ede3e37463cd89eb050640ce8db890321c
anderground777 <anderground@inbox.ru>
2022-11-23 13:40:45 +0300

    finish

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,19 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
+// Объявим две переменные, первая переменная будет получать элемент по названию класса accordion. Вторая переменная с названием i
+let acc = document.getElementsByClassName("accordion");
+let i;
+// В цикле for перебираем весь массив элементов с классом accordion, то есть будут выводиться все заголовки.
 for (i = 0; i < acc.length; i++) {
+    // На каждый из элементов добавляем действие по клику, будет выполняться функция, когда ко всем элементам в массиве применяется метод toggle.
     acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+        // Метод toggle добавляет и удаляет класс active для замены цвета кнопки и поворота крестика управляющей панелью.
         this.classList.toggle("active");
 
-        /* Toggle between hiding and showing the active panel */
+        // Назначаем переменной panel – свойство nextElementSibling, это переключатель между скрытием и показом активной панели.
         var panel = this.nextElementSibling;
+        
         if (panel.style.display === "block") {
             panel.style.display = "none";
+            
         } else {
             panel.style.display = "block";
         }
